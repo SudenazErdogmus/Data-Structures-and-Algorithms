9e465c69f2c9b6459f63049efc30ace8471cb360
Sudenaz Erdoğmuş <133746647+SudenazErdogmus@users.noreply.github.com>
2025-10-20 14:33:18 +0300

    Create MyArray.java

---
+++ DataStructuresLabExercises/DataStructuresLabWeek1/MyArray.java
@@ -0,0 +1,73 @@
+public class MyArray {
+    private int[] array;
+    
+    public MyArray(int[] array){
+        this.array = array;
+    }
+    
+    public void display(){
+        for(int i=0; i<array.length-1; i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+    
+    public int min(){
+        int minimumNumber = array[0];
+        for(int i=1; i<array.length-1; i++){
+            if(minimumNumber > array[i]){
+                minimumNumber = array[i];
+            }
+        }
+        return minimumNumber;
+    }
+    
+    public int lineerSearch(int number){
+        for(int i=0; i<array.length-1; i++){
+            if(number == array[i]){
+                return number;
+            }
+        }
+        return -1;
+    }
+    
+    public void bubbleSort(){
+   
+      for(int i=0; i<array.length-1; i++){
+          for(int j=0; j<array.length-1; j++){
+              if(array[j+1] < array[j]){
+                  int temp = array[j];
+                  array[j] = array[j+1];
+                  array[j+1] = temp;
+              }
+          }
+          
+          System.out.println((i+1) + ". tur sonucu");
+          display();
+          
+      }
+    }
+    
+    public int binarySearch(int number){
+       
+        int start = 0;
+        int middle ;
+        int end = array.length-1;
+        
+        while(start <= end){
+            middle = (0+array.length-1)/2;
+            if(number == array[middle]){
+                System.out.println("Tebrikler, tek attiniz <333");
+                return number;
+            }
+            else if(number < array[middle]){
+                end = middle - 1;
+            }
+            else{
+                start = middle + 1;
+            }
+        }  
+        return number;
+    }
+    
+}
+ 
